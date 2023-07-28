4ee94b0fcfb96ee4712f2af1b02c3134b351d278
Leo <hank8142000@yahoo.com.tw>
2023-07-28 19:03:32 +0800

    home page rightContainer complete

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -199,7 +199,77 @@ const HomePage = () => {
         </div>
       </div>
       
-      <div className="rightContainer"></div>
+      <div className="rightContainer">
+        <div className="rightContainer-area">
+          <div className="rightContainer-area-title">推薦跟隨</div>
+          <div className="rightContainer-area-followList">
+            <LogoIcon src={logo} alt="" className="followList-img" />
+            <div className="followList-content">
+              <div className="followList-content-username">Pizza Hut</div>
+              <div className="followList-content-account">@ouzzahut</div>
+            </div>
+            <Button className="followList-content-btn">正在跟隨</Button>
+          </div>
+          <div>
+            <div className="rightContainer-area-followList">
+              <LogoIcon src={logo} alt="" className="followList-img" />
+              <div className="followList-content">
+                <div className="followList-content-username">Pizza Hut</div>
+                <div className="followList-content-account">@ouzzahut</div>
+              </div>
+              <Button className="followList-content-btn">正在跟隨</Button>
+            </div>
+            <div className="rightContainer-area-followList">
+              <LogoIcon src={logo} alt="" className="followList-img" />
+              <div className="followList-content">
+                <div className="followList-content-username">Pizza Hut</div>
+                <div className="followList-content-account">@ouzzahut</div>
+              </div>
+              <Button className="followList-content-btn">正在跟隨</Button>
+            </div>
+            <div className="rightContainer-area-followList">
+              <LogoIcon src={logo} alt="" className="followList-img" />
+              <div className="followList-content">
+                <div className="followList-content-username">Pizza Hut</div>
+                <div className="followList-content-account">@ouzzahut</div>
+              </div>
+              <Button className="followList-content-btn">正在跟隨</Button>
+            </div>
+            <div className="rightContainer-area-followList">
+              <LogoIcon src={logo} alt="" className="followList-img" />
+              <div className="followList-content">
+                <div className="followList-content-username">Pizza Hut</div>
+                <div className="followList-content-account">@ouzzahut</div>
+              </div>
+              <Button className="followList-content-btn">正在跟隨</Button>
+            </div>
+            <div className="rightContainer-area-followList">
+              <LogoIcon src={logo} alt="" className="followList-img" />
+              <div className="followList-content">
+                <div className="followList-content-username">Pizza Hut</div>
+                <div className="followList-content-account">@ouzzahut</div>
+              </div>
+              <Button className="followList-content-btn">正在跟隨</Button>
+            </div>
+            <div className="rightContainer-area-followList">
+              <LogoIcon src={logo} alt="" className="followList-img" />
+              <div className="followList-content">
+                <div className="followList-content-username">Pizza Hut</div>
+                <div className="followList-content-account">@ouzzahut</div>
+              </div>
+              <Button className="followList-content-btn">正在跟隨</Button>
+            </div>
+            <div className="rightContainer-area-followList">
+              <LogoIcon src={logo} alt="" className="followList-img" />
+              <div className="followList-content">
+                <div className="followList-content-username">Pizza Hut</div>
+                <div className="followList-content-account">@ouzzahut</div>
+              </div>
+              <Button className="followList-content-btn">正在跟隨</Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
