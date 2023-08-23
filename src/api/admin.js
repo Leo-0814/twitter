1556f3197a93a4baf0da37a25a469b9d792fa591
Leo <hank8142000@yahoo.com.tw>
2023-08-23 18:09:07 +0800

    add admin login api

---
+++ src/api/admin.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+import Swal from "sweetalert2"
+
+const baseUrl = 'https://adminapi.ball188.cc'
+
+export const login = async({account, password, gcode}) => {
+  try {
+    const res = await axios.post(`${baseUrl}/admin/login`, {account, password, gcode})
+
+    const token = res.data.data.token
+    
+    if(token) {
+      return token
+    }
+
+  } catch (error) {
+    const errorMessage = error.response.data.result.message
+    Swal.fire({
+      icon: 'error',
+      title: '登入失敗',
+      text: errorMessage
+    })
+    console.error('[login error]', error)
+  }
+}
